1d9cc8e07e31cc53b500e89019e939473958bf43
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-04-18 19:26:29 -0600

    chore: linear search

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,4 +1,14 @@
 export default function linear_search(
     haystack: number[],
     needle: number,
-): boolean {}
+): boolean {
+    for (let i = 0; i <= haystack.length; i++) {
+        const item = haystack[i];
+
+        if (item === needle) {
+            return true;
+        }
+    }
+
+    return false;
+}
