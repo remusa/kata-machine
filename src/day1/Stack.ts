b6d2e0ad1104f5a3049a41fc178a183b322da192
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-04-24 01:18:53 -0600

    chore: stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,54 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const newNode: Node<T> = {
+            value: item,
+        };
 
-}
+        this.length++;
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        newNode.prev = this.head;
+        this.head = newNode;
+    }
+
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
 
-}
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
+
+        const head = this.head as Node<T>;
+
+        if (this.length === 0) {
+            this.head = undefined;
+        } else {
+            this.head = head.prev;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
