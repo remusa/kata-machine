b0eec0e468dab0020125589d521c0fcce15d405b
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-04-27 15:25:47 -0600

    chore: implement quicksort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,1 +1,34 @@
-export default function quick_sort(arr: number[]): void {}
+function qs(arr: number[], lo: number, hi: number) {
+	if (lo >= hi) {
+		return;
+	}
+
+	const pivotIdx = partition(arr, lo, hi);
+
+	qs(arr, lo, pivotIdx - 1);
+	qs(arr, pivotIdx + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number) {
+	const pivot = arr[hi];
+	let idx = lo - 1;
+
+	for (let i = lo; i < hi; i++) {
+		if (arr[i] <= pivot) {
+			idx++;
+			const temp = arr[i];
+			arr[i] = arr[idx];
+			arr[idx] = temp;
+		}
+	}
+
+	idx++;
+	const temp = arr[idx];
+	arr[idx] = arr[hi];
+	arr[hi] = temp;
+
+	return idx;
+}
+export default function quick_sort(arr: number[]): void {
+	qs(arr, 0, arr.length - 1);
+}
