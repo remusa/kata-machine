94fa77dfdc105f0737e58367f2173d3b86e92087
R Sánchez <9169831+remusa@users.noreply.github.com>
2024-04-29 23:31:18 -0600

    chore: add bfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,1 +1,21 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {}
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<number> | undefined | null)[] = [head]
+
+  while (q.length) {
+    const curr = q.shift() as BinaryNode<number> | undefined | null
+    if (!curr) {
+      continue
+    }
+
+    // search
+    if (curr.value === needle) {
+      return true
+    }
+
+    // enqueue children
+    q.push(curr.left)
+    q.push(curr.right)
+
+    return false
+  }
+}
